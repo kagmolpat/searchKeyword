f0900bde3b1393131c20268d2b257f961fcd8a44
Kamolpat Kasemkijkajon <kamolpat.kag@gmail.com>
2021-06-25 16:19:45 +0700

    test serialize (not work)

--- insert.py
+++ insert.py
@@ -1,46 +1,56 @@
 import json
-from sqlalchemy.sql.functions import current_date
 from app import db
-from models import SearchKeyword, CountKeyword, datetime, AlchemyEncoder
+from models import SearchKeyword, CountKeyword, AlchemyEncoder
 from sqlalchemy import func
 from flask import jsonify
+import datetime
+
 
 db.create_all()
 
 # add new row to SearchKeyword
-f = SearchKeyword(keyword="delete", search_datetime=datetime.utcnow())
-db.session.add(f)
+# f = SearchKeyword(keyword="add", search_datetime=datetime.utcnow())
+# db.session.add(f)
 
 result = SearchKeyword.query.all()
 
 # count no of keyword
-ct = db.session.query(func.count('*')).select_from(SearchKeyword).scalar()
+ct = db.session.query(func.count(SearchKeyword.keyword)).distinct().scalar()
+print("keyword list: ")
+print(ct)
 
-key = db.session.query(SearchKeyword.keyword).distinct().all()
+# select keyword from search_keyword
+keywords = db.session.query(SearchKeyword.keyword).distinct().all()
+print("keyword list: ")
+print(keywords)
 
+# count query result
 cd = db.session.query(func.count(SearchKeyword.keyword), SearchKeyword.keyword)\
+    .filter(SearchKeyword.search_datetime == datetime.datetime(2021, 6, 25))\
     .group_by(SearchKeyword.keyword).order_by(func.current_date()).all()
+# type of query result
+print("type of count result: ")
+print(type(cd))
+print("count word result (cd): ")
+print(cd)
+print("convert to JSON")
+# sk = CountKeyword(count_date=datetime.utcnow(), count_output=cd.to_json())
+# print(sk)
+# print(json.dumps(sk, cls=AlchemyEncoder))
 
-# filter_by(SearchKeyword.search_datetime == datetime(2021, 6, 21))
 
 # jsonStr = dumps(cd)
 
-# c = CountKeyword(count_output=jsonify(cd), count_date=func.current_date())
-# db.session.add(c
+# c = CountKeyword(count_output=cd.to_json(), count_date=func.current_date())
+# print("result c:" + c)
+# db.session.add(c)
 
 # r = jsonify(list(cd))
 # print(r[1])
 
 # print(result)
 
-print(key)
-# type of query result
-print(type(cd))
-# count query result
-print(cd)
-
 # print(json.dumps(cd, cls=AlchemyEncoder))
 
-print(ct)
 
 db.session.commit()
